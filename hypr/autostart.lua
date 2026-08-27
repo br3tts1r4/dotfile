fc3b98e1757466eb73e90ff5030292eb6215db0e
br3tts1r4 <kd_zillza2000_tyz9@hotmail.com>
2026-08-27 23:27:51 +1000

    chore: removed waybar

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -11,9 +11,8 @@ local waybar = os.getenv("HOME") .. "/.config/waybar/launch.sh"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
-  hl.exec_cmd(waybar)
-  hl.exec_cmd("hyprpaper")
-  --   hl.exec_cmd(terminal)
-  --   hl.exec_cmd("nm-applet")
-  --   hl.exec_cmd("waybar & hyprpaper & firefox")
+	hl.exec_cmd("hyprpaper")
+	--   hl.exec_cmd(terminal)
+	--   hl.exec_cmd("nm-applet")
+	--   hl.exec_cmd("waybar & hyprpaper & firefox")
 end)
