ed39cae790a159f9fd609fdd36a48d160a3d84c0
br3tts1r4 <kd_zillza2000_tyz9@hotmail.com>
2026-08-27 23:40:35 +1000

    chore: clearned-up all waybar

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -5,7 +5,6 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 -- Set path config
-local waybar = os.getenv("HOME") .. "/.config/waybar/launch.sh"
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
@@ -14,5 +13,4 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hyprpaper")
 	--   hl.exec_cmd(terminal)
 	--   hl.exec_cmd("nm-applet")
-	--   hl.exec_cmd("waybar & hyprpaper & firefox")
 end)
